12a0b5e05e8ba9778feba1cd49d0bd3f4372b939
Joe Higgins <joehiggins.bass@gmail.com>
2018-03-27 15:54:27 -0400

    Initial Commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+// Header file
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>Header here</h1>
+   </body>
+ </html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: Joe Higgins First Theme
+Other stuff
+*/
+?>
+<!DOCTYPE html >
+<html>
+<head>
+  <title>Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style/style.css"/>
+</head>
+
+<body>
+  <div class="full-page-wrap parallax">
+    <div class="grid-wrap">
+      <div class="row page-top">
+        <div class="col-12 col-m-12 " >
+        12 col, 12 col
+          <div class="head-shot">
+            <img src="./images/sam-singing.png" alt="Sam singing"/>
+          </div>
+        </div>
+      </div>
+      <div class="row page-bottom">
+        <div class="col-4 col-m-4 " >
+        <ul>
+          <li>menu 1</li>
+          <li>menu 2</li>
+          <li>menu 3</li>
+        </ul>
+        </div>
+        <div class="col-8 col-m-8">
+            1 col, 4 col
+        </div>
+
+      </div>
+      <div class="row footer">
+        <div class="col-3 col-m-3 " >
+        3 col, 3 col
+        </div>
+        <div class="col-6 col-m-6">
+            6 col, 6 col
+        </div>
+        <div class="col-3 col-m-3">
+          3 col, 3 col
+        </div>
+
+      </div>
+    </div>
+  </div>
+</body>
